3557c0f4041f70f967bab256be834da90917e61d
Andrii <not4me2009@gmail.com>
2020-07-06 01:59:09 +0300

    update

--- project/api/__init__.py
+++ project/api/__init__.py
@@ -25,9 +25,6 @@ def create_app(script_info=None):
     from project.api.ping import ping_blueprint
     app.register_blueprint(ping_blueprint)
 
-    from project.api.users import users_blueprint
-    app.register_blueprint(users_blueprint)
-
     # shell context for flask cli
     @app.shell_context_processor
     def ctx():
